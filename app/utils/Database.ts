9e52cd27c41310a568e45d9f6f8c14102fe87be3
MicronGit <micron.git@gmail.com>
2025-09-21 00:23:21 +0900

    refactor: update database connection method and improve SQL scripts for consistency

--- app/utils/Database.ts
+++ app/utils/Database.ts
@@ -1,4 +1,11 @@
 import mysql from 'mysql2/promise';
 
 export const getDatabase = async () =>
-  await mysql.createConnection('mysql://root:rootpassword@localhost:3306/localdb');
+  // 'mysql://root:rootpassword@localhost:3306/localdb'
+  await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'rootpassword',
+    database: 'localdb',
+    charset: 'utf8mb4',
+  });
